5d716b3f29c72bf722adf08c76ff7e87fcc66264
ixpert <amr.elnagar@outlook.com>
2018-12-30 15:37:36 +0100

    Initial commit

---
+++ src/main/java/com/ixpert/ilms/IlmsApplication.java
@@ -0,0 +1,14 @@
+package com.ixpert.ilms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IlmsApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/ixpert/ilms/IlmsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ixpert.ilms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IlmsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
